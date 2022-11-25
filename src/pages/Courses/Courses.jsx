432714c33900945a496d2319edcc473e72b63bd9
Zaid Ahmad <ZaidAhmad-dev@users.noreply.github.com>
2022-11-24 21:16:35 -0800

    Added google tag

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,8 +1,38 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import CourseList from '../../components/courses/CourseList';
 import Search from "../../components/search/Search";
+import { useContext } from 'react';
 import './courses.css';
+import { CourseContext } from '../../Contexts/CourseContext';
 
 const Courses = () => {
+
+	useEffect(() => {
+	// https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLB03EA9545DD188C3&key=AIzaSyCCe49ArBE3ZDLgQJeQOnYZgbOQigeadFc
+	const options = {
+			method: 'GET',
+			url: 'https://youtube.googleapis.com/youtube/v3/playlistItems',
+			params: {
+				part: 'snippet',
+				maxResults: 50,
+				playlistId: 'PLB03EA9545DD188C3',
+				key: import.meta.env.VITE_YOUTUBE_API_KEY
+			}
+		};
+
+		axios.request(options).then(function (response) {
+			console.log(response.data);
+		}
+		).catch(function (error) {
+			console.error(error);
+		}
+		);
+		
+
+
+
+	}, []);
 
 	return (
 		<>
